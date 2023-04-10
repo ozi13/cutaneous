9961c9eee5ca5b243bcf0a05af411d97879bd354
Ozioma Ozor-Ilo <71114453+ozi13@users.noreply.github.com>
2023-04-10 14:40:43 -0400

    Add files via upload
    Uploaded the existing files here.

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function expandTactileResearch() {
+    var x = document.getElementById("expand-tactile");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function expandThermalResearch() {
+    var x = document.getElementById("expand-thermal");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function expandSkinResearch() {
+    var x = document.getElementById("expand-skin");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
